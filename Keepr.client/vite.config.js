d9e1f5bf1e83d4a3db7e71a527c738d13dc4779c
AlexMuir138 <muirkat138@gmail.com>
2021-08-03 16:33:00 -0600

    go live

--- Keepr.client/vite.config.js
+++ Keepr.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: '../keepr/wwwroot',
+    outDir: '../Keepr/wwwroot',
     sourcemap: false
   },
   server: {
---
+++ Keepr/wwwroot/assets/AboutPage.271a4aaf.js
@@ -0,0 +1,1 @@
+import{o as e,a as r,b as n}from"./vendor.55f09c39.js";const o={},s=n("h1",null,"This is the profile page",-1);o.render=function(n,o,t,a,i,l){return e(),r("div",null,[s])};export default o;
